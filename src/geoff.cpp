46f319e8ab1050086ce9c01d02b3ffd036bbfa6e
Evan Mortimore <evan-github@madstyle.net>
2023-03-12 06:19:38 +0000

    New geoff.h: common header upcoming separate class files.

--- src/geoff.cpp
+++ src/geoff.cpp
@@ -14,34 +14,7 @@
 //------------------------------------------------------------------------------
 
 #include "geoff_certificate.hpp"
-
-#include <boost/beast/core.hpp>
-#include <boost/beast/http.hpp>
-#include <boost/beast/ssl.hpp>
-#include <boost/beast/websocket.hpp>
-#include <boost/beast/version.hpp>
-#include <boost/asio/bind_executor.hpp>
-#include <boost/asio/dispatch.hpp>
-#include <boost/asio/signal_set.hpp>
-#include <boost/asio/steady_timer.hpp>
-#include <boost/asio/strand.hpp>
-#include <boost/make_unique.hpp>
-#include <boost/optional.hpp>
-#include <algorithm>
-#include <cstdlib>
-#include <functional>
-#include <iostream>
-#include <memory>
-#include <string>
-#include <thread>
-#include <vector>
-
-namespace beast = boost::beast;                 // from <boost/beast.hpp>
-namespace http = beast::http;                   // from <boost/beast/http.hpp>
-namespace websocket = beast::websocket;         // from <boost/beast/websocket.hpp>
-namespace net = boost::asio;                    // from <boost/asio.hpp>
-namespace ssl = boost::asio::ssl;               // from <boost/asio/ssl.hpp>
-using tcp = boost::asio::ip::tcp;               // from <boost/asio/ip/tcp.hpp>
+#include "geoff.h"
 
 // Return a reasonable mime type based on the extension of a file.
 beast::string_view
