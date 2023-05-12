09375fa15f117a7cce8ab3d7a0a92a6660447cb7
Evan Mortimore <evan-github@madstyle.net>
2023-05-12 16:26:18 -0700

    Fix merge screw-up.

--- src/Asteroids.cpp
+++ src/Asteroids.cpp
@@ -456,7 +456,7 @@ void Player::TickEvent(AsteroidsSession& session)
 	txBuff->set_uint8(0xBB);
 	txBuff->set_uint8(static_cast<uint8_t>(WebsockSession::MessageType_t::PlayerTickMessage));
 	txBuff->set_uint32(session.SessionID());
-	txBuff->set_uint32(session.GetTimerTick());
+	txBuff->set_uint32(g_universe->GetTimerTick());
 
 	txBuff->set_uint16(Context::width);
 	txBuff->set_uint16(Context::height);
@@ -614,7 +614,7 @@ void Universe::PerSessionTickEvent(AsteroidsSession& session)
 	txBuff->set_uint8(0xBB);
 	txBuff->set_uint8(static_cast<uint8_t>(WebsockSession::MessageType_t::UniverseTickMessage));
 	txBuff->set_uint32(session.SessionID());
-	txBuff->set_uint32(session.GetTimerTick());
+	txBuff->set_uint32(g_universe->GetTimerTick());
 	txBuff->set_uint16(Context::width);
 	txBuff->set_uint16(Context::height);
 
